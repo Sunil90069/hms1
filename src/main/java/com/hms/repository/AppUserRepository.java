e43ee497c7fa0dfdc25a3529f0af102c616619b2
sunil <sk367114@gamil.com>
2024-10-23 16:54:19 +0530

    new project HMS

---
+++ src/main/java/com/hms/repository/AppUserRepository.java
@@ -0,0 +1,14 @@
+package com.hms.repository;
+
+import com.hms.entity.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+
+
+    Optional<AppUser>findByUsername(String username);
+    Optional<AppUser>findByEmail(String email);
+}
